d88ce52f9249c9bb60e430d8f96a6d68c47a613a
Sasu Paakkunainen <sasupa@gmail.com>
2019-09-11 13:23:21 +0300

    updated package, procfile, hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
@@ -19,6 +20,6 @@ http.createServer(function (req, res) {
         console.log("Incoming request... " + req.url);
         res.end();
     })
-}).listen(8080);
+}).listen(PORT);
 
 console.log("Server listening on port 8080...")
